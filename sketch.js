ada8c51ee7a48ad68a8586e5c8a41ace0e3042ae
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-10-15 10:46:47 -0400

    initial commit

---
+++ sketch.js
@@ -0,0 +1,85 @@
+//0s and 1s to print on a6 sheets.
+
+//a6 at 300 dpi.
+let pw = 298;
+let ph = 420;
+
+let side_margins = 30;
+let edge_margins = 100;
+
+//font sizes:
+const foreground = 10;
+const max_background = 4.5;
+
+//font variables:
+let serif; //noto.
+let mono; // source.
+
+//colour variables:
+let red = "#c02126";
+let grey = "#bcbec0";
+let bg = 255;
+let black = 0;
+
+let binaries = [];
+
+let pdf; 
+
+function preload() {
+  serif = loadFont("./noto-serif-hebrew_regular.ttf");
+  mono = loadFont("./source-code-pro_medium.ttf");
+}
+
+function setup() {
+  createCanvas(pw, ph);
+
+  //set defaults:
+  noStroke();
+
+  //create binaries:
+  for (let x = side_margins; x <= width - side_margins; x += max_background) {
+    for (let y = edge_margins; y <= height - edge_margins; y += max_background) {
+      binaries.push(new Binary(x, y));
+    }
+  }
+
+  pdf = createPDF(); 
+  pdf.beginRecord(); 
+}
+
+function draw() {
+  background(bg);
+
+  //display all binaries:
+  for (let binary of binaries) {
+    binary.display();
+  }
+  pdf.save(); 
+  noLoop(); 
+}
+
+class Binary {
+  constructor(x, y, w, h) {
+    //construction is the same as a rectangle.
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+
+    this.t = random(["0", "1"]);
+  }
+  display() {
+    // push();
+
+    //set defaults:
+    textAlign(LEFT, TOP);
+
+    fill(grey);
+
+    textFont(mono);
+    textSize(max_background);
+    text(this.t, this.x, this.y);
+
+    // pop();
+  }
+}
